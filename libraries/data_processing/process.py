9493b780e5b54c2f623a33be3577d54bfc673c12
UcefMountacer <youssefmountassir16@gmail.com>
2022-01-29 02:20:10 +0100

    just saving, no progress

--- libraries/data_processing/process.py
+++ libraries/data_processing/process.py
@@ -2,6 +2,8 @@
 import cv2
 import os
 import numpy as np
+import skimage.measure
+import matplotlib.patches as mpatches
 
 def read_video(video_file_path):
 
@@ -44,7 +46,7 @@ def segment_background(im):
     '''
     remove background using color segmentation
     '''
-
+    
     lower = np.array([180, 48, 35])
     upper = np.array([245, 145, 128])
     thresh = cv2.inRange(im, lower, upper)
@@ -54,6 +56,108 @@ def segment_background(im):
     result = cv2.bitwise_and(im, im, mask=mask)
 
     return result
+
+
+def adjust_gamma(image, gamma):
+
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+        for i in np.arange(0, 256)]).astype("uint8")
+
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+
+def image_enhance(img):
+
+    # CLAHE (Contrast Limited Adaptive Histogram Equalization)
+    clahe = cv2.createCLAHE(clipLimit=3., tileGridSize=(8,8))
+
+    # convert from BGR to LAB color space
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)  
+    
+    # split on 3 different channels
+    l, a, b = cv2.split(lab)  
+
+    # apply CLAHE to the L-channel
+    l2 = clahe.apply(l)  
+
+    # merge channels
+    lab = cv2.merge((l2,a,b))  
+    
+    # convert from LAB to BGR
+    img_enhance = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)  
+    
+    return img_enhance
+
+
+def correct_gamma(image):
+    
+    # apply gamma correction and show the images
+    gamma = 2
+    gamma = gamma if gamma > 0 else 0.1
+    adjusted = adjust_gamma(image, gamma=gamma)
+    enhanced_image = image_enhance(adjusted)
+
+    return enhanced_image
+
+
+def remove_blur(im):
+
+    sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    sharpen = cv2.filter2D(im, -1, sharpen_kernel)
+
+    return sharpen
+
+
+def mask_root(image):
+
+    cropped_image = image[0:580, 700:1200]
+   
+    # switch to HSV color space, better to pick intervals than RGB
+    hsv = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2HSV)
+    
+    # specify the lower and upper boundaries for the color segmentation
+    low= np.array([0, 129, 173])
+    high = np.array([121, 255, 255])
+
+    # take the foreground with the specified boundaries
+    mask = cv2.inRange(hsv, low, high)
+
+    # label = skimage.measure.label(mask)
+    # prop = skimage.measure.regionprops(label)            
+    
+    # apply it on the original frame and get its original form
+    res = cv2.bitwise_not(cropped_image,cropped_image, mask= mask)
+
+    return mask
+
+if __name__ == '__main__':
+
+    # directories
+    v1 = 'data/43/MVI_0252.MOV'
+    v2 = 'data/43/MVI_0590.MOV'
+    
+    output_dir = 'libraries/depth/output'
+    # input data+
+    list1 = read_video(v1)
+    list2 = read_video(v2)
+
+    l = []
+
+    for i, (im1,im2) in enumerate(zip(list1,list2)):
+
+        # im1 = remove_blur(correct_gamma(im1))
+        mask = mask_root(im1)
+
+        mask = cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
+
+        l.append(mask)
+
+    
+    generate_video(l, '/media/youssef/ubuntu_data/mask.MOV')
 
 
 
--- libraries/depth/stereo_depth.py
+++ libraries/depth/stereo_depth.py
@@ -23,7 +23,7 @@ def load_params(path):
     return camera_matrix
 
 
-def init_stereo_method(minDisparity = 0 , numDisparities = 16*45, blockSize = 1, lmbda = 40000, sigma = 2):
+def init_stereo_method(minDisparity = 0 , numDisparities = 16*35, blockSize = 1, lmbda = 10000, sigma = 3):
 
     '''
     initialize stereo matchers nd filters
@@ -138,123 +138,124 @@ def run_on_stereo(left , right, methods, base, focal, max_depth= 500 , min_depth
     return depth, depth_viz, disp
 
 
+if __name__ == '__main__':
 
 
+    def generate_video(frames, output_dir):
 
-
-
-# def generate_video(frames, output_dir):
-
-#     '''
-#     generate and save a video from a list of frames
-#     '''
-   
-#     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-#     height, width= frames[0].shape[0], frames[0].shape[1]
-#     video = cv2.VideoWriter(output_dir, fourcc, 20, (width,height))
-
-#     for frame in frames:
-
-#         video.write(frame)
-
-#     cv2.destroyAllWindows()
-#     video.release()
-
-# def read_video(video_file_path):
-
-#     list_of_frames = []
-#     vidcap = cv2.VideoCapture(video_file_path)
-
-#     success,image = vidcap.read()
-
-#     while success:
-#         list_of_frames.append(image)
-#         success,image = vidcap.read()
-
-#     return list_of_frames
-
-# def adjust_gamma(image, gamma):
-#     # build a lookup table mapping the pixel values [0, 255] to
-#     # their adjusted gamma values
-#     invGamma = 1.0 / gamma
-#     table = np.array([((i / 255.0) ** invGamma) * 255
-#         for i in np.arange(0, 256)]).astype("uint8")
- 
-#     # apply gamma correction using the lookup table
-#     return cv2.LUT(image, table)
-
-# def image_enhance(img):
-
-#     # CLAHE (Contrast Limited Adaptive Histogram Equalization)
-#     clahe = cv2.createCLAHE(clipLimit=3., tileGridSize=(8,8))
-
-#     # convert from BGR to LAB color space
-#     lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)  
+        '''
+        generate and save a video from a list of frames
+        '''
     
-#     # split on 3 different channels
-#     l, a, b = cv2.split(lab)  
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        height, width= frames[0].shape[0], frames[0].shape[1]
+        video = cv2.VideoWriter(output_dir, fourcc, 20, (width,height))
 
-#     # apply CLAHE to the L-channel
-#     l2 = clahe.apply(l)  
+        for frame in frames:
 
-#     # merge channels
-#     lab = cv2.merge((l2,a,b))  
+            video.write(frame)
+
+        cv2.destroyAllWindows()
+        video.release()
+
+    def read_video(video_file_path):
+
+        list_of_frames = []
+        vidcap = cv2.VideoCapture(video_file_path)
+
+        success,image = vidcap.read()
+
+        while success:
+            list_of_frames.append(image)
+            success,image = vidcap.read()
+
+        return list_of_frames
+
+    def adjust_gamma(image, gamma):
+        # build a lookup table mapping the pixel values [0, 255] to
+        # their adjusted gamma values
+        invGamma = 1.0 / gamma
+        table = np.array([((i / 255.0) ** invGamma) * 255
+            for i in np.arange(0, 256)]).astype("uint8")
     
-#     # convert from LAB to BGR
-#     img_enhance = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)  
+        # apply gamma correction using the lookup table
+        return cv2.LUT(image, table)
+
+    def image_enhance(img):
+
+        # CLAHE (Contrast Limited Adaptive Histogram Equalization)
+        clahe = cv2.createCLAHE(clipLimit=3., tileGridSize=(8,8))
+
+        # convert from BGR to LAB color space
+        lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)  
+        
+        # split on 3 different channels
+        l, a, b = cv2.split(lab)  
+
+        # apply CLAHE to the L-channel
+        l2 = clahe.apply(l)  
+
+        # merge channels
+        lab = cv2.merge((l2,a,b))  
+        
+        # convert from LAB to BGR
+        img_enhance = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)  
+        
+        return img_enhance
+
+    def correct_gamma(image):
+        
+        # apply gamma correction and show the images
+        gamma = 5
+        gamma = gamma if gamma > 0 else 0.1
+        adjusted = adjust_gamma(image, gamma=gamma)
+        enhanced_image = image_enhance(adjusted)
+
+        return enhanced_image
+
+    def remove_blur(im):
+
+        sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        sharpen = cv2.filter2D(im, -1, sharpen_kernel)
+
+        return sharpen
     
-#     return img_enhance
 
-# def correct_gamma(image):
+    # directories
+    v1 = 'data/43/MVI_0252.MOV'
+    v2 = 'data/43/MVI_0590.MOV'
     
-#     #get size of image
-#     img_height, img_width = image.shape[:2]
+    output_dir = 'libraries/depth/output'
+    # input data+
+    list1 = read_video(v1)[:10]
+    list2 = read_video(v2)[:10]
+
+    # list1 = list(map(correct_gamma , list1))
+    # list2 = list(map(correct_gamma , list2))
+
+    # list1 = list(map(remove_blur, list1))
+    # list2 = list(map(remove_blur, list2))
+
+    methods = init_stereo_method()
+
+    depth_list = []
+
+    for i, (im1,im2) in enumerate(zip(list1,list2)):
+
+        print('step ', i)
+
+        # m = mask(list1[i],list1[i+1])
+
+        depth, depth_viz, disp = run_on_stereo(im1 , im2, methods,base=100, focal=730, max_depth= 500 , min_depth = 0)    
+
+        # depth = segment_background(depth, m)
+
+        depth_list.append(depth_viz)
+
+        # if i == len(list1) - 2:
+        #     break
     
-#     # apply gamma correction and show the images
-#     gamma = 1.5
-#     gamma = gamma if gamma > 0 else 0.1
-#     adjusted = adjust_gamma(image, gamma=gamma)
-#     enhanced_image = image_enhance(adjusted)
-
-#     return enhanced_image
-
-    
-
-# if __name__ == '__main__':
-
-
-#     # directories
-#     v1 = 'data/43/MVI_0252.MOV'
-#     v2 = 'data/43/MVI_0590.MOV'
-    
-#     output_dir = 'libraries/depth/output'
-#     # input data+
-#     list1 = read_video(v1)[:10]
-#     list2 = read_video(v2)[:10]
-
-#     list1 = list(map(correct_gamma , list1))
-#     list2 = list(map(correct_gamma , list2))
-
-#     methods = init_stereo_method()
-
-#     depth_list = []
-
-#     for i, (im1,im2) in enumerate(zip(list1,list2)):
-
-#         print('step ', i)
-
-#         # m = mask(list1[i],list1[i+1])
-
-#         depth, depth_viz, disp = run_on_stereo(im1 , im2, methods,base=100, focal=730, max_depth= 500 , min_depth = 0)    
-
-#         # depth = segment_background(depth, m)
-
-#         depth_list.append(depth_viz)
-
-#         # if i == len(list1) - 2:
-#         #     break
-    
-#     generate_video(depth_list, '/media/youssef/ubuntu_data/WORK/Roots_Point_Cloud/outputs/depth/depth_enhanced.MOV')
+    # generate_video(depth_list, '/media/youssef/ubuntu_data/WORK/Roots_Point_Cloud/outputs/depth/depth_enhanced.MOV')
 
 
 
